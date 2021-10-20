a5b6a97b6a24e36d780b1276a10519f6cdc52d36
Alex Powell <alexanderp@agilitycis.com>
2021-10-20 19:58:11 +1100

    step 5

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Layout from "./Layout";
 import { Head } from "@inertiajs/inertia-react";
+import { format, parse } from "date-fns";
 
 const Home = ({ time }) => {
     return (
         <>
             <Head title="Welcome" />
             <h1 className="text-xl font-bold mb-4">Welcome to Inertia</h1>
-            <p>Hello, it's {time}</p>
+            {/* <p>Hello, it's {time}</p> */}
+            <p>Hello, it's {format(parse(time, 'yyyy-MM-dd HH:mm:ss', new Date()), "h 'o''clock'")}</p>
         </>
     );
 };
