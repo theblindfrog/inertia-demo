860c856fb3d2704aacf272b608535b9c433be9c8
Alex Powell <alexanderp@agilitycis.com>
2021-10-20 20:00:34 +1100

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    @routes
   </head>
   <body class="font-sans text-gray-900 antialiased bg-gray-50">
     @inertia
